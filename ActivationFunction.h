2ef6ac3934f8bc05d5c07bbf34e46737b32dad0d
= <=>
2024-07-29 12:33:48 +0300

    add activation function class

---
+++ ActivationFunction.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <functional>
+#include <vector>
+
+namespace neural_network {
+
+class ActivationFunction {
+public:
+    static ActivationFunction ReLU();
+    static ActivationFunction LeakyReLU();
+    static ActivationFunction Sigmoid();
+    ActivationFunction(std::function<double(double)>&& f0,
+                       std::function<double(double)>&& f1);
+    double evaluate0(double x) const;
+    double evaluate1(double x) const;
+    std::vector<double> evaluate0(const std::vector<double>& x) const;
+    std::vector<double> evaluate1(const std::vector<double>& x) const;
+
+private:
+    static constexpr double LeakyReLUCoefficient = 0.01;
+
+    const std::function<double(double)> f0_;
+    const std::function<double(double)> f1_;
+};
+
+}  // namespace neural_network
